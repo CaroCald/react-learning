1293553048294d5e4d655c0c5a0dd4f6b1511afb
Carolina Calderon <carolina.2594@hotmail.com>
2026-05-28 18:56:28 -0500

    init teslo shop

---
+++ 07-telslo-shop/src/TesloShopApp.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from 'react-router';
+import { appRouter } from './app.router';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+export const TesloShopApp = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+};
---
+++ 07-telslo-shop/src/shop/actions/get-produtcs.action.ts
@@ -0,0 +1,33 @@
+import { tesloApi } from "@/api/tesloApi";
+import type { ProductsResponse } from "@/interface/products.response";
+
+
+interface Options {
+    limit?: number | string;
+    offset?: number | string;
+
+
+}
+export const getProductsAction = async (options: Options): Promise<ProductsResponse> => {
+
+    const { limit, offset } = options;
+
+    const { data } = await tesloApi.get<ProductsResponse>('/products', {
+        params: {
+            limit,
+            offset
+        }
+    });
+
+    const productsWithImageUrl = data.products.map(product => {
+        return {
+            ...product,
+            images: product.images.map(image => `${import.meta.env.VITE_API_URL}/files/product/${image}`)
+        }
+    })
+
+    return {
+        ...data,
+        products: productsWithImageUrl
+    };
+}
